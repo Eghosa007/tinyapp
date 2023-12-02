f1071b3c5c02e620134086c9d129054060909715
Eghosa <perezonline300@gmail.com>
2023-12-02 00:53:08 -0500

    update

--- express_server.js
+++ express_server.js
@@ -1,61 +1,46 @@
-// express_server.js
-
-const express = require("express");
+// Import required modules
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs");
+// Set up middleware
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const urlDatabase = {};
+// Sample URL database
+const urlDatabase = {
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com',
+  // ... other URLs
+};
 
-app.use(express.urlencoded({ extended: true }));
-
-// Route to render the form
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-// Route to handle form submission
-app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL;
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
-
-// Route to handle shortURL requests
-app.get("/urls/:id", (req, res) => {
+// GET route to render the index page
+app.get('/urls', (req, res) => {
   const templateVars = {
-    shortURL: req.params.id,        // Assign the shortURL parameter from the request to templateVars
-    longURL: urlDatabase[req.params.id],  // Retrieve the corresponding longURL from the urlDatabase
-    id: req.params.id,              // Pass the id variable
+    urls: urlDatabase,
   };
-  res.render("urls_show", templateVars);  // Render the 'urls_show' template with templateVars
+  res.render('urls_index', templateVars);
 });
 
-
-// Route to handle redirection for shortURLs
-app.get("/u/:id", (req, res) => {
+// POST route to handle URL deletion
+app.post('/urls/:id/delete', (req, res) => {
   const shortURL = req.params.id;
-  const longURL = urlDatabase[shortURL];
-  if (longURL) {
-    res.redirect(longURL);
+
+  // Check if the URL exists in the database before attempting deletion
+  if (urlDatabase[shortURL]) {
+    // Use the delete operator to remove the URL
+    delete urlDatabase[shortURL];
+    console.log(`Deleted URL with shortURL: ${shortURL}`);
   } else {
-    res.status(404).send("Short URL not found");
+    console.log(`URL with shortURL: ${shortURL} not found`);
   }
+
+  // Redirect back to the urls_index page
+  res.redirect('/urls');
 });
 
-// Additional function to generate random string
-function generateRandomString() {
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
-
+// Listen on the specified port
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
